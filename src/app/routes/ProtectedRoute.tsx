2ef2eb4e219522338511d659c8590a18f4506928
IamNeoleon <kusherbaev.bakhtiyar@gmail.com>
2026-03-10 11:19:38 +0500

    refactor: add loader for protected route

--- src/app/routes/ProtectedRoute.tsx
+++ src/app/routes/ProtectedRoute.tsx
@@ -1,11 +1,16 @@
 import { useAuth } from '@/shared/hooks/use-auth'
+import { Spinner } from '@/shared/ui/shadcn/spinner'
 import { Navigate, Outlet, useLocation } from 'react-router'
 
 export const ProtectedRoute = () => {
    const { isLoading, isAuthenticated } = useAuth()
    const location = useLocation()
 
-   if (isLoading) return null
+   if (isLoading) return (
+      <div className='h-screen flex justify-center items-center'>
+         <Spinner className='size-12' />
+      </div>
+   )
 
    if (!isAuthenticated) {
       return <Navigate to="/auth" replace state={{ from: location }} />
